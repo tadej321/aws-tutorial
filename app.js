314aac246517aaf308f7483177722ed965f7d710
tadejkosovel <tedy.kosovel39@gmail.com>
2021-03-31 17:02:27 +0200

    third commit

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ const express = require('express');
 
 
 const app = express();
+const port = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
   res.send('Its working boyyyyyy');
 })
 
-app.listen(4000, () => {
+app.listen(port, () => {
   console.log('hello')
 })
