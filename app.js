2be3e4fa122d739f2ee265a72ca0ddfa43418fe3
tadejkosovel <tedy.kosovel39@gmail.com>
2021-04-02 14:36:17 +0200

    .

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = 8080 || 8080;
 
 app.get('/', (req, res) => {
   res.send('Its working boyyyyyy');
